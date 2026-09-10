8c37ab9d540b6e7771988323808ceb3543fc1463
Rayan Silva Lima <rayan.lima@wiserxp.com>
2026-09-09 22:22:02 -0300

    feat/melhoria

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'node:path';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  envPrefix: 'FIREBASE_',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
